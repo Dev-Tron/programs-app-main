334c5bd0d0819f772b9d8fbbb4683f94cb2a9a9a
Developer Tron <84941942+Dev-Tron@users.noreply.github.com>
2023-10-22 06:02:31 -0400

    test

--- src/components/loginform.jsx
+++ src/components/loginform.jsx
@@ -21,7 +21,7 @@ export default function LoginForm() {
   
     try {
       // Make a POST request to the /api/login route
-      const response = await fetch('https://programs-app-main.vercel.app/api/login', {
+      const response = await fetch('/api/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
